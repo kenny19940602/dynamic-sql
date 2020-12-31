39a6c697db14cf5773df1a18b38bcd1411d7c244
金龙-汇码科技 <kenny19940602@163.com>
2020-12-31 17:20:07 +0800

    [add] 动态SQl

--- src/main/java/com/kenny/DynamicSqlApplication.java
+++ src/main/java/com/kenny/DynamicSqlApplication.java
@@ -3,7 +3,7 @@ package com.kenny;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(proxyBeanMethods = false)
 public class DynamicSqlApplication {
 
     public static void main(String[] args) {
